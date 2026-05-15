f272ff5fe3af6027d3e2d895a5904d87d20e6eac
kielbassa <karol.narel@icloud.com>
2026-05-15 16:38:44 +0200

    Revert "Added AES encryption"
    This reverts commit 6eadfee59800cdd6562619a2f10227bece868b26.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@
 #include <Adafruit_SSD1306.h>
 #include <graphics.h>
 
-// AES libraries
-#include "encryption_aes.h"
-
 // LoRa libraries
 #include <LoRa.h>
 #include <SPI.h>
@@ -56,7 +53,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 BluetoothSerial SerialBT;
 
 // Global variables
-const String deviceName = "ESP32_OLED-1";
+const String deviceName = "ESP32_OLED-2";
 
 String btBuffer      = "";   // buffer for incoming BT data
 String messageToSend = "";   // message waiting in outbox to be sent via LoRa
@@ -169,14 +166,11 @@ void transition(AppState newState){
 }
 
 void sendViaLoRa(const String& msg){
-  String encrypted = encryptAES(msg);
   LoRa.beginPacket();
-  LoRa.print(encrypted);
+  LoRa.print(msg);
   LoRa.endPacket();
-  Serial.println("LoRa TX : " + msg);
-  Serial.println("LoRa TX (encrypted): " + encrypted);
+  Serial.println("LoRa TX: " + msg);
   SerialBT.println("LoRa TX OK: " + msg);
-  SerialBT.println("LoRa TX (encrypted): " + encrypted);
 }
 
 void receiveViaLoRa(){
@@ -187,10 +181,7 @@ void receiveViaLoRa(){
       incoming += (char)LoRa.read();
     }
     Serial.println("LoRa RX: " + incoming);
-    String decrypted = decryptAES(incoming);
-    Serial.println("LoRa RX (decrypted): " + decrypted);
     SerialBT.println("LoRa RX: " + incoming);
-    SerialBT.println("LoRa RX (decrypted): " + decrypted);
     lastReceived = incoming;
   }
 }
